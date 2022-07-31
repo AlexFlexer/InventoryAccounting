fc183dd172b4921c99bc62557a5d2609986591f8
Александр Дяченко <a.dyachenko@tirscript.com>
2022-07-31 22:00:44 +0300

    MainViewModel.kt: common API

---
+++ app/src/main/java/com/magjed/inventoryaccounting/ui/MainViewModel.kt
@@ -0,0 +1,18 @@
+package com.magjed.inventoryaccounting.ui
+
+import androidx.lifecycle.ViewModel
+import com.magjed.inventoryaccounting.database.ProductEntity
+
+class MainViewModel: ViewModel() {
+  fun removeItem(id: String) {
+
+  }
+
+  fun addItem(item: ProductEntity) {
+
+  }
+
+  fun filter(item: ProductEntity?) {
+
+  }
+}
