47795ea7c82511bfe9adbd025d6f3cad35c3f9f0
Александр Дяченко <a.dyachenko@tirscript.com>
2022-07-31 18:02:15 +0300

    Constants.kt: database-related constants

---
+++ app/src/main/java/com/magjed/inventoryaccounting/database/Constants.kt
@@ -0,0 +1,14 @@
+/*
+ * This file contains database-related constants, such as table
+ * names, database names and so on.
+ */
+package com.magjed.inventoryaccounting.database
+
+//#region table names
+const val TABLE_HW_ITEMS = "hardware_items"
+const val TABLE_LOGS = "spy_log"
+//#endregion
+
+//#region database names
+const val DATABASE_INVENTORY_ACCOUNTING = "inv_acc"
+//#endregion
