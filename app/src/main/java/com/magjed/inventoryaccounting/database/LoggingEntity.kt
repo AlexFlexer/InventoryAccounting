07287d4f687ca1a13964aea21ecbfb39286f361c
Александр Дяченко <a.dyachenko@tirscript.com>
2022-07-31 18:46:10 +0300

    LoggingEntity.kt: entities and DAO for logs table

---
+++ app/src/main/java/com/magjed/inventoryaccounting/database/LoggingEntity.kt
@@ -0,0 +1,60 @@
+package com.magjed.inventoryaccounting.database
+
+import androidx.room.Embedded
+import androidx.room.PrimaryKey
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * This is which changes are allowed on database items.
+ */
+enum class ModificationType {
+  /**
+   * Added new item.
+   */
+  ADDED,
+
+  /**
+   * Removed item.
+   */
+  REMOVED,
+
+  /**
+   * Changed any of the attributes of the item.
+   */
+  CHANGED
+}
+
+/**
+ * Log entity, represents a logged change in the database.
+ *
+ * @param time is a string-encoded time when the event has happened.
+ * @param type defines what has changed in database.
+ * @param oldProduct is old version of item (not null if [type] is [ModificationType.CHANGED] or
+ * [ModificationType.REMOVED], null otherwise).
+ * @param newProduct is the currently presented in the database item (null only if [type] is
+ * [ModificationType.REMOVED]).
+ *
+ * @author Alexander Dyachenko
+ */
+data class LoggingEntity(
+  @PrimaryKey(autoGenerate = true) val id: Int,
+  val time: String,
+  val type: ModificationType,
+  @Embedded val oldProduct: ProductEntity?,
+  @Embedded val newProduct: ProductEntity?
+)
+
+/**
+ * DAO for interacting with logs table.
+ *
+ * @author Alexander Dyachenko
+ */
+interface LogDao {
+
+  @Query("select * from $TABLE_LOGS")
+  fun getAllLogs(): Flow<LoggingEntity>
+
+  @Query("delete from $TABLE_LOGS")
+  fun removeLogs()
+}
