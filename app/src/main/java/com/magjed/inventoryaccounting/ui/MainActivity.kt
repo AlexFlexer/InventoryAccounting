83d92558c25da3ba8e16ef2c332d7131d18c2be2
Александр Дяченко <a.dyachenko@tirscript.com>
2022-07-31 17:24:39 +0300

    MainActivity.kt, activity_main.xml: temp commit

---
+++ app/src/main/java/com/magjed/inventoryaccounting/ui/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.magjed.inventoryaccounting.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.magjed.inventoryaccounting.R
+import com.magjed.inventoryaccounting.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+  private val mBinding: ActivityMainBinding by viewBinding()
+}
