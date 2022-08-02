0ba8bc4e86604e8aa7bca785c293f0c7e7eb0334
Александр Дяченко <a.dyachenko@tirscript.com>
2022-08-02 21:34:57 +0300

    Utils.kt: View.layoutInflater

--- app/src/main/java/com/magjed/inventoryaccounting/utils/Utils.kt
+++ app/src/main/java/com/magjed/inventoryaccounting/utils/Utils.kt
@@ -2,8 +2,11 @@ package com.magjed.inventoryaccounting.utils
 
 import android.app.Activity
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
 import android.widget.Toast
 import androidx.annotation.StringRes
+import androidx.core.content.getSystemService
 import androidx.fragment.app.Fragment
 import androidx.room.Room
 import androidx.room.RoomDatabase
@@ -16,7 +19,7 @@ import androidx.room.RoomDatabase
  * @param customBuilderSetup is a function to be invoked for custom parameters application
  * on the given [RoomDatabase.Builder] object.
  */
-inline fun <reified D: RoomDatabase> Context.getOrCreateDatabase(
+inline fun <reified D : RoomDatabase> Context.getOrCreateDatabase(
   dbName: String = D::class.java.name,
   customBuilderSetup: RoomDatabase.Builder<D>.() -> Unit = {}
 ): D {
@@ -48,3 +51,9 @@ fun Fragment.toast(text: CharSequence) = activity?.toast(text)
  * Shows the given [textRes] as toast message.
  */
 fun Fragment.toast(@StringRes textRes: Int) = activity?.toast(textRes)
+
+/**
+ * Gets [LayoutInflater] service from [View] context.
+ */
+val View.layoutInflater: LayoutInflater
+  get() = this.context.getSystemService()!!
