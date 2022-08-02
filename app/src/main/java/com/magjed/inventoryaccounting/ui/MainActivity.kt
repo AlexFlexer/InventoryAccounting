ea54a1a42e9d1e902b8b891d9b64622dd73cd8af
Александр Дяченко <a.dyachenko@tirscript.com>
2022-08-02 21:56:42 +0300

    MainViewModel.kt is moved from file to MainActivity.kt

--- app/src/main/java/com/magjed/inventoryaccounting/ui/MainActivity.kt
+++ app/src/main/java/com/magjed/inventoryaccounting/ui/MainActivity.kt
@@ -2,9 +2,25 @@ package com.magjed.inventoryaccounting.ui
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.magjed.inventoryaccounting.R
+import com.magjed.inventoryaccounting.database.ProductEntity
 import com.magjed.inventoryaccounting.databinding.ActivityMainBinding
+
+class MainViewModel: ViewModel() {
+  fun removeItem(id: String) {
+
+  }
+
+  fun addItem(item: ProductEntity) {
+
+  }
+
+  fun filter(item: ProductEntity?) {
+
+  }
+}
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
