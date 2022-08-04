d019d4eb43b7b2bb3a3c3b0b5e1a5d03559d489f
Александр Дяченко <a.dyachenko@tirscript.com>
2022-08-04 21:34:27 +0300

    ItemInfoDialog.kt: now ready to be used when null item is passed

--- app/src/main/java/com/magjed/inventoryaccounting/ui/ItemInfoDialog.kt
+++ app/src/main/java/com/magjed/inventoryaccounting/ui/ItemInfoDialog.kt
@@ -8,12 +8,13 @@ import by.kirich1409.viewbindingdelegate.viewBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.magjed.inventoryaccounting.database.ProductEntity
 import com.magjed.inventoryaccounting.databinding.DialogItemInfoBinding
-import com.magjed.inventoryaccounting.utils.argument
+import com.magjed.inventoryaccounting.utils.argumentNullable
+import com.magjed.inventoryaccounting.utils.getContent
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
 class ItemInfoDialog : BottomSheetDialogFragment() {
 
-  val mItem: ProductEntity by argument()
+  val mItem: ProductEntity? by argumentNullable()
   private val mBinding: DialogItemInfoBinding by viewBinding()
   private val mViewModel: MainViewModel by sharedViewModel()
 
@@ -27,13 +28,24 @@ class ItemInfoDialog : BottomSheetDialogFragment() {
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
-    mBinding.inputId.setText(mItem.id)
-    mBinding.inputManufacturer.setText(mItem.manufacturer)
-    mBinding.inputModel.setText(mItem.model)
-    mBinding.inputType.setText(mItem.type)
-    mBinding.inputAmount.setText(mItem.amount)
-    mBinding.inputLocation.setText(mItem.location)
-    mBinding.btnSave.setOnClickListener { mViewModel.addItem(mItem); dismiss() }
-    mBinding.btnDelete.setOnClickListener { mViewModel.removeItem(mItem.id); dismiss() }
+    mBinding.inputId.setText(mItem?.id)
+    mBinding.inputManufacturer.setText(mItem?.manufacturer)
+    mBinding.inputModel.setText(mItem?.model)
+    mBinding.inputType.setText(mItem?.type)
+    mBinding.inputAmount.setText(mItem?.amount?.toString())
+    mBinding.inputLocation.setText(mItem?.location)
+    mBinding.btnSave.setOnClickListener { mViewModel.addItem(collectItemInfo()); dismiss() }
+    mBinding.btnDelete.setOnClickListener { mViewModel.removeItem(mItem?.id ?: "0"); dismiss() }
+  }
+
+  private fun collectItemInfo(): ProductEntity {
+    return ProductEntity(
+      mBinding.inputId.getContent(),
+      mBinding.inputType.getContent(),
+      mBinding.inputModel.getContent(),
+      mBinding.inputManufacturer.getContent(),
+      mBinding.inputLocation.getContent(),
+      mBinding.inputAmount.getContent().toIntOrNull() ?: 0
+    )
   }
 }
